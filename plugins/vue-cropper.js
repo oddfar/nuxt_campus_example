e2eab8cfd2d682eed0b7645db342643f15f32657
oddfar <oddfar@163.com>
2023-03-30 22:42:38 +0800

    :tada:nuxt项目独立出来

---
+++ plugins/vue-cropper.js
@@ -0,0 +1,14 @@
+// import Vue from "vue";
+// import { VueCropper } from "vue-cropper";
+// Vue.use(VueCropper)
+
+
+// Vue =  require('vue')
+// if(process.browser) {
+//   vueCropper = require('vue-cropper')
+//   Vue.use(vueCropper.default)
+// }
+
+import Vue from 'vue'
+import { VueCropper } from "vue-cropper"
+Vue.component('VueCropper', VueCropper)
